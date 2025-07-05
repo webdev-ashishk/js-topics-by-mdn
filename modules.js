205fe48e2c47b6e6a387fd0bbdf7522baa580663
webdev-ashishk <webdev.ashishk@gmail.com>
2025-07-05 07:42:33 +0530

    solving modules

---
+++ modules.js
@@ -0,0 +1,6 @@
+export const sum = (a, b) => {
+  return a + b;
+};
+export const mull = (a, b) => {
+  return a * b;
+};
---
+++ trailingCommas.js
@@ -0,0 +1,6 @@
+import { sum as anju, mull as ashish } from './modules.js';
+
+let sumResult = anju(10, 30);
+let mullResult2 = ashish(10, 30);
+console.log(mullResult2);
+console.log(sumResult);
